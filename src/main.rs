56a946edd218e693409c7fcd3776de1bbafbb3a0
watcol <kantaa058@gmail.com>
2020-11-03 22:43:24 +0900

    Add blief code generator.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate qrcode;
+
+use qrcode::QrCode;
+
 fn main() {
     println!("Hello, world!");
+    let code = QrCode::new("Hello, world!").unwrap();
+    let s = code.render()
+        .dark_color("\x1b[40m  \x1b[m")
+        .light_color("\x1b[47m  \x1b[m")
+        .build();
+    println!("{}", s);
 }

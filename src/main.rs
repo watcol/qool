2168cf89ba261ef6915b27bd24c6d0fceb2ff2cb
watcol <kantaa058@gmail.com>
2020-11-04 22:32:00 +0900

    Improved terminal color

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+extern crate colored;
 extern crate qrcode;
 
+use colored::Colorize;
 use qrcode::QrCode;
 
 fn main() {
     println!("Hello, world!");
     let code = QrCode::new("Hello, world!").unwrap();
-    let s = code.render()
-        .dark_color("\x1b[40m  \x1b[m")
-        .light_color("\x1b[47m  \x1b[m")
+    let s = code
+        .render()
+        .dark_color("  ".on_black().to_string().as_ref())
+        .light_color("  ".on_white().to_string().as_ref())
         .build();
     println!("{}", s);
 }

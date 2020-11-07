2c2df48fceef4f4c23c3be9c60c403f42598a7f1
watcol <kantaa058@gmail.com>
2020-11-07 14:08:03 +0900

    Add command line argument.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ mod format;
 
 use crate::format::{Format, Output};
 use qrcode::QrCode;
+use std::env::args;
 use std::io::{stdout, Write};
 
 fn main() {
-    println!("Hello, world!");
-    let code = QrCode::new(b"Hello, world!").unwrap();
+    let original = args().nth(1).unwrap_or("Hello, World!".to_string());
+    println!("Original: {}", original);
+    let code = QrCode::new(original).unwrap();
     let s = code.output(Format::Term);
     stdout().write(&s).expect("Failed to write.");
 }

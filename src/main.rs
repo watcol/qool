9fe1bdde98b6f2dcbeb138bb83d92b8f6fed7fc8
watcol <potfman@gmail.com>
2021-01-27 21:50:40 +0900

    Implement simple "cat".

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use std::io::{stdin, Read, Result as IORes};
+
+fn main() -> IORes<()> {
+    // Read from stdin.
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf)?;
+
+    // DEBUG
+    print!("{}", buf);
+
+    Ok(())
+}

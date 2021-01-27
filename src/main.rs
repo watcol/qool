989614bbd696fe90a3d2ed3dc521504da0c20d94
watcol <potfman@gmail.com>
2021-01-27 21:56:03 +0900

    Introduced logger.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+#[macro_use]
+extern crate log;
+extern crate fmtlog;
+
 use std::io::{stdin, Read, Result as IORes};
 
 fn main() -> IORes<()> {
+    //fmtlog::default()
+    fmtlog::new(fmtlog::Config::new().level(log::LevelFilter::Trace))  // DEBUG
+        .set()
+        .unwrap();
+
     // Read from stdin.
     let mut buf = String::new();
     stdin().read_to_string(&mut buf)?;
 
     // DEBUG
-    print!("{}", buf);
+    debug!("buffer:\n{}", buf);
 
     Ok(())
 }
